379373571b3a7abfb2cdeeb8aca9c92563dde3fa
Micah Winkelspecht <winkelspecht@gmail.com>
2012-08-29 16:04:41 -0700

    Add optional :data parameter.

--- lib/kontagent/tracking.rb
+++ lib/kontagent/tracking.rb
@@ -60,6 +60,7 @@ module Kontagent
       path += "&st1=#{opts[:st1]}" if opts[:st1]
       path += "&st2=#{opts[:st2]}" if opts[:st2]
       path += "&st3=#{opts[:st3]}" if opts[:st3]
+      path += "&data=#{opts[:data]}" if opts[:data]
       call_api(path)
     end
     
